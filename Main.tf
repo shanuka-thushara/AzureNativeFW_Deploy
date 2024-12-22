246eff013488705389598cb67de394f4b49b2733
Shanuka Thushara <shanukathushara@gmail.com>
2024-12-22 21:05:36 +0300

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = "testpip"
+  name                = "AzureFirewallPublicIP"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
