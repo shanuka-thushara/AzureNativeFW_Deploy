1aaf06c1321471ff2c9c9eff9b32c6ad24d6dde8
Shanuka Thushara <shanukathushara@gmail.com>
2024-12-26 15:18:31 +0300

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "example" {
-  name     = "Test-RG"
+  name     = "AzureFirewall-RG"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "AzureFirewall-VNet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.50.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "example" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.50.1.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
@@ -37,4 +37,10 @@ resource "azurerm_firewall" "example" {
     subnet_id            = azurerm_subnet.example.id
     public_ip_address_id = azurerm_public_ip.example.id
   }
+}
+
+resource "azurerm_firewall_policy" "example" {
+  name                = "FW-Policy"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
